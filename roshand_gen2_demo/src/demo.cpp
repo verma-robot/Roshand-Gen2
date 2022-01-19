109cc4bc8f4a338ba1ff9a2ec212679fe37712fb
Verma Robotics <support@verma-robot.com>
2022-01-19 17:03:17 +0800

    Add files via upload

--- roshand_gen2_demo/src/demo.cpp
+++ roshand_gen2_demo/src/demo.cpp
@@ -2,28 +2,34 @@
 #include <actionlib/client/simple_action_client.h>
 #include "roshand_gen2_msgs/HandCommandAction.h"    
 
-bool finished;
  
 typedef actionlib::SimpleActionClient<roshand_gen2_msgs::HandCommandAction> Client; 
 
 void doneaction(const actionlib::SimpleClientGoalState& state, const roshand_gen2_msgs::HandCommandResultConstPtr& result)
 {
-    ROS_INFO("Goal arrived");
-    finished = result -> finish;
+
+    //if(result -> finish == true)ROS_INFO("Goal arrived....");
+    //else ROS_WARN("Can't arrive the Goal, please retry...");
+
+
 }
  
 
 void activeaction()
 {
-
-    ROS_INFO("Goal arriving...................");
+    ROS_INFO("Get a new Goal............");
 }
  
 
 void feedbackaction(const roshand_gen2_msgs::HandCommandFeedbackConstPtr& feedback)
 {
+    bool resl;
+    roshand_gen2_msgs::HandCommandFeedback get_back;
+    get_back = *feedback;
+    resl = get_back.state;
 
 
+    //std::cout << (int16_t)(resl)<< std::endl;
 }
  
 int main(int argc, char** argv)
@@ -38,52 +44,115 @@ int main(int argc, char** argv)
     ROS_INFO("Action server started, sending goal.");
 
     roshand_gen2_msgs::HandCommandGoal demo_goal; 
-    finished = false;
     ROS_INFO("Open the hand without sensor.....................");
     demo_goal.use_sensor = false;
-    demo_goal.goal_finger_distance = 80;    
+    demo_goal.target_finger_distance = 50;    
 
     client.sendGoal(demo_goal,  doneaction, activeaction, feedbackaction);
 
-    while(!finished){};
-    finished = false;    
+    client.waitForResult(ros::Duration(5.0));
+    if(client.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
+    {
+        ROS_INFO("Arrived the goal.....");
+    }
+    else
+    {
+
+        ROS_WARN("Can't arrive the goal....");
+        ROS_WARN("Cancle the action....");
+        client.cancelAllGoals();
+ 
+    }
+
     ROS_INFO("Close the hand without sensor...................");
     demo_goal.use_sensor = false;
-    demo_goal.goal_finger_distance = 2;    
+    demo_goal.target_finger_distance = 2;    
 
     client.sendGoal(demo_goal,  doneaction, activeaction, feedbackaction);
 
+    client.waitForResult(ros::Duration(5.0));
+    if(client.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
+    {
+        ROS_INFO("Arrived the goal.....");
+    }
+    else
+    {
 
-    while(!finished){};
-    finished = false;    
-    ROS_INFO("Close the hand with sensor.......................");
-    demo_goal.use_sensor = true;
-    demo_goal.sensor_threadhold = 10;
-    demo_goal.close_step_mag = 8;
-
-    client.sendGoal(demo_goal,  doneaction, activeaction, feedbackaction);
+        ROS_WARN("Can't arrive the goal....");
+        ROS_WARN("Cancle the action....");
+        client.cancelAllGoals();
+ 
+    }
 
 
-
-    while(!finished){};
-    finished = false;    
-    ROS_INFO("Close the hand with sensor.......................");
-    demo_goal.use_sensor = true;
-    demo_goal.sensor_threadhold = 10;
-    demo_goal.close_step_mag = 8;
-
-    client.sendGoal(demo_goal,  doneaction, activeaction, feedbackaction);
-
-    while(!finished){};
-    finished = false;
     ROS_INFO("Open the hand without sensor.....................");
     demo_goal.use_sensor = false;
-    demo_goal.goal_finger_distance = 80;    
+    demo_goal.target_finger_distance = 80;    
 
     client.sendGoal(demo_goal,  doneaction, activeaction, feedbackaction);
 
-    while(!finished){};
-    finished = false;
+
+    client.waitForResult(ros::Duration(5.0));
+    if(client.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
+    {
+        ROS_INFO("Arrived the goal.....");
+    }
+    else
+    {
+
+        ROS_WARN("Can't arrive the goal....");
+        ROS_WARN("Cancle the action....");
+        client.cancelAllGoals();
+ 
+    }
+
+    ROS_INFO("Close the hand with sensor.......................");
+    demo_goal.use_sensor = true;
+    demo_goal.sensor_threshold = 10;
+    demo_goal.close_step_mag = 16;
+
+    client.sendGoal(demo_goal,  doneaction, activeaction, feedbackaction);
+
+
+    client.waitForResult(ros::Duration(5.0));
+    if(client.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
+    {
+        ROS_INFO("Arrived the goal.....");
+    }
+    else
+    {
+
+        ROS_WARN("Can't arrive the goal....");
+        ROS_WARN("Cancle the action....");
+        client.cancelAllGoals();
+ 
+    }
+
+    ROS_INFO("Open the hand without sensor.....................");
+    demo_goal.use_sensor = false;
+    demo_goal.target_finger_distance = 80;    
+
+    client.sendGoal(demo_goal,  doneaction, activeaction, feedbackaction);
+
+
+    client.waitForResult(ros::Duration(5.0));
+    if(client.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
+    {
+        ROS_INFO("Arrived the goal.....");
+        ros::shutdown();
+
+    }
+    else
+    {
+
+        ROS_WARN("Can't arrive the goal....");
+        ROS_WARN("Cancle the action....");
+        client.cancelAllGoals();
+        ros::shutdown();
+
+ 
+    }
+
 
     ros::spin();
  
